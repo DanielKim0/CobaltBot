bab1731d9261de4887dede94662f8ce9700a791c
Daniel Kim <danielynskim@gmail.com>
2020-10-23 20:01:02 -0400

    Start on eu4 cog and make templates for bot

--- discord/bot.py
+++ discord/bot.py
@@ -1,4 +1,3 @@
-# bot.py
 import os
 import random
 import discord
@@ -12,6 +11,10 @@ class CobaltBot(commands.Bot):
         load_dotenv()
         self.token = os.getenv('DISCORD_TOKEN')
         self.serv = os.getenv('DISCORD_SERVER')
+        self.default_prefix = prefix
+        self.prefixes = dict()
+        # Certain cogs should be admitted for different servers.
+        self.cog_dict = dict()
 
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
@@ -30,6 +33,13 @@ class CobaltBot(commands.Bot):
             f'Hi {member.name}, welcome to my Discord server!'
         )
 
+    async def determine_prefix(self, message):
+        guild = message.guild
+        if guild:
+            return self.prefixes[guild]
+        else:
+            return self.default_prefix
+
     async def on_error(self, event, *args, **kwargs):
         with open('err.log', 'a') as f:
             if event == 'on_message':
@@ -37,16 +47,29 @@ class CobaltBot(commands.Bot):
             else:
                 raise
 
+    @commands.command(name="help")
+    async def help(self, pass_context=True):
+        # TODO
+        pass
+
+    @commands.command(name="get_prefix", aliases=["prefix", "pre"])
+    async def get_prefixes(self, ctx, name):
+        pass
+
+    @commands.command(name="set_prefix")
+    async def set_prefixes(self, ctx, name):
+        pass
+
     @commands.command(name="add_cog", pass_context=True)
     async def add_cog(self, ctx, name):
-        super().add_cog(name)
+        self.add_cog(name)
 
     @commands.command(name="remove_cog", pass_context=True)
     async def remove_cog(self, ctx, name):
-        super().remove_cog(name)
+        self.remove_cog(name)
 
     def run(self):
-        super().run(self.token)
+        self.run(self.token)
 
 bot = CobaltBot()
 bot.run()
---
+++ discord/cogs/eu4.py
@@ -0,0 +1,14 @@
+import os
+import random
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+class EU4Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def cog_check(self, ctx)
+
+    @commands.command(name=idea, aliases=[])
+    async def 

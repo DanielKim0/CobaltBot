182ba4b1178dc8c6198f09019bcc4fb30d56987c
Daniel Kim <danielynskim@gmail.com>
2020-11-20 21:29:42 -0500

    Add functionality to extract all demon links from main page

--- processing/smt/demon.py
+++ processing/smt/demon.py
@@ -2,20 +2,21 @@ from requests_html import HTMLSession
 import json
 
 class SMT_Demon_Parser:
-    def __init__(self):
-        self.demon_list = "https://aqiu384.github.io/megaten-fusion-tool/?csr=/smt4/demons"
+    def __init__(self, game):
+        self.url = "https://aqiu384.github.io/megaten-fusion-tool/"
+        self.game = game
 
     def parse_demon_list(self):
+        demon_list = self.url + "?csr=/" + self.game + "/demons"
         session = HTMLSession()
-        res = session.get(self.demon_list)
+        res = session.get(demon_list)
         res.html.render()
         found = res.html.find("tr.app-smt-demon-list-row")
-        for item in found:
-            print([i.text for i in item.find("td")])
+        return [self.url + self.game + "/demons/" + item.find("td")[2].text for item in found]
 
     def main(self):
-        self.parse_demon_list()
+        names = self.parse_demon_list()
 
 if __name__ == "__main__":
-    p = SMT_Demon_Parser()
+    p = SMT_Demon_Parser("smt4")
     p.main()
